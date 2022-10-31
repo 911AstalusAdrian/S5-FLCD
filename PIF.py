5af93f90abec3d5fbd586a2aa6b24a9cc6b353dc
Adrian Astalus <astalus.adrian1703@gmail.com>
2022-10-31 19:57:04 +0200

    changed representation

--- PIF.py
+++ PIF.py
@@ -19,13 +19,13 @@ class PIF:
             tokens = list(filter(None, re.split(';| |: |{ | }|,|\n', line)))
             for token in tokens:
                 if token in self.tokens_list:
-                    self.pif.append((token, self.st.get_token_index(token)))
+                    self.pif.append((-1, self.st.get_token_index(token)))
                 elif self.is_constant(token):
                     self.pif.append((self.get_constant_id(), self.st.get_constant_position(token)))
                 elif self.is_identifier(token):
                     self.pif.append((self.get_identifier_id(), self.st.get_identifier_position(token)))
                 else:
-                    self.pif.append((token, -1))
+                    continue
 
     def is_constant(self, token_to_check):
         return self.st.check_constant(token_to_check)
@@ -45,7 +45,7 @@ class PIF:
 
     def __str__(self):
         string = "------P.I.F.------\n"
-        string += "element type --- position in its ST\n"
+        string += "element type | position in its list\n"
         for pif_elem in self.pif:
-            string += str(pif_elem[0]) + "---" + str(pif_elem[1]) + "\n"
+            string += str(pif_elem[0]) + " --- " + str(pif_elem[1]) + "\n"
         return string
